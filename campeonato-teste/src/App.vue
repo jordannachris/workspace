073bbe690c6d983e8b3336c9838bac0a4b0e66d3
Gustavo Nachbar <gnachbar.breve@gmail.com>
2021-03-12 17:37:18 -0300

    adicionando top bar

--- campeonato-teste/src/App.vue
+++ campeonato-teste/src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <AppToBar/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+import AppTopBar from './components/shared/AppTopBar.vue'
+export default {
+  name: 'App',
+  components:{
+    AppTopBar
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
